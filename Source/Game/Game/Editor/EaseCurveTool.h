5359e5e7ebfb1d59e7dedc5c3e055ceaa525f921
EIntemporel <dev.intemporel@gmail.com>
2023-08-16 20:28:44 +0200

    Updating EaseCurveTool

--- Source/Game/Game/Editor/EaseCurveTool.h
+++ Source/Game/Game/Editor/EaseCurveTool.h
@@ -15,8 +15,19 @@ namespace Editor
         virtual void Update(f32 deltaTime) override;
 
     private:
+        f32 GetEasePoint(f32 t, i32 interpolationType, i32 interpolationName, f32 polynomialDegree = 2.0f);
+
         void ResetTimer();
         void ResetMinMax();
+
+        void DrawHeader();
+        void Draw2DPreview();
+        void Draw1DPreview();
+
+    private:
+        const ImU32 _whiteFullColor = IM_COL32(255, 255, 255, 255);
+        const ImU32 _whiteHalfColor = IM_COL32(255, 255, 255, 100);
+        const ImU32 _redColor = IM_COL32(255, 0, 0, 255);
 
         bool _preview1D = true;
         bool _preview2D = true;
@@ -30,6 +41,19 @@ namespace Editor
         f32 _maxY = 0.0f;
         f32 _minY = 10.0f;
 
-        f32 GetEasePoint(f32 t, i32 n0, i32 n1, i32 polynomialDegree = 2);
+        bool _useBouncing = true;
+        i32 _segmentCount = 30;
+        f32 _canvasScale = 0.6f;
+        f32 _invHalfCanvasScale = 0.2f;
+        f32 _polynomialDegree = 2;
+
+        static const u8 _easeTypeCount = 3;
+        const char* const _easeType[_easeTypeCount] = {"In", "Out", "InOut"};
+        i32 _selectedEaseType = 0;
+
+        static const u8 _easeNameCount = 14;
+        const char* const _easeName[_easeNameCount] = {"Sine", "Circ", "Elastic", "Expo", "Back", "Bounce",
+            "Quadratic", "Cubic", "Quartic", "Quintic", "Sextic", "Septic", "Octic", "Polynomial"};
+        i32 _selectedEaseName = 0;
     };
 }
