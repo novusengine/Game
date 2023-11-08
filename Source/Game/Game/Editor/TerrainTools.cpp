4a1db2f497cddf327c3ed520d32d658e2d231718
Fraxinus94 <keithlekgolo@gmail.com>
2023-11-07 19:23:39 -0500

    Terrain Tools GUI Update
    Added:
    - Brush Types Enums
    - ColumnCheckbox Function
    - GroupHeader Function
    - HoveredMouseWheelStep Func
    - Updated Terrain Tool UI

--- Source/Game/Game/Editor/TerrainTools.cpp
+++ Source/Game/Game/Editor/TerrainTools.cpp
@@ -14,13 +14,32 @@ namespace Editor
 	{
 		if (ImGui::Begin(GetName()))
 		{
-			Util::Imgui::ColorPicker("Vertex Color:", &_vertexColor);
+			Util::Imgui::GroupHeader("Brush Types");
+
+			if (ImGui::BeginTable("Brush Type Table", 2))
+			{
+				Util::Imgui::ColumnCheckBox("Flatten", _brushTypes, BrushTypes::FLAT, BrushTypes::COUNT);
+				Util::Imgui::ColumnCheckBox("Smooth", _brushTypes, BrushTypes::SMOOTH, BrushTypes::COUNT);
+				Util::Imgui::ColumnCheckBox("Linear", _brushTypes, BrushTypes::LINEAR, BrushTypes::COUNT);
+				Util::Imgui::ColumnCheckBox("Vertex", _brushTypes, BrushTypes::VERTEX, BrushTypes::COUNT);
+
+				ImGui::EndTable();
+			}
+
+			ImGui::NewLine();
+
+			Util::Imgui::GroupHeader("Brush Settings");
 
 			Util::Imgui::FloatSlider("Hardness:", &_brushHardness, 0.0f, 100.0f, 0.01f, 0.1f, true);
 			Util::Imgui::FloatSlider("Radius:", &_brushRadius, 0.0f, 100.0f, 0.01f, 0.1f, true);
 			Util::Imgui::FloatSlider("Pressure:", &_brushPressure, 0.0f, 100.0f, 0.01f, 0.1f, true);
-		}
 
+			ImGui::NewLine();
+
+			Util::Imgui::GroupHeader("Vertex Color Picker");
+
+			Util::Imgui::ColorPicker("Vertex Color:", &_vertexColor, ImVec2(100.0f, 100.0f));
+		}
 		ImGui::End();
 	}
 }
--- Source/Game/Game/Editor/TerrainTools.h
+++ Source/Game/Game/Editor/TerrainTools.h
@@ -8,6 +8,16 @@ namespace Editor
 	class TerrainTools : public BaseEditor
 	{
 	public:
+		enum BrushTypes
+		{
+			FLAT = 0,
+			SMOOTH,
+			LINEAR,
+			VERTEX,
+			COUNT
+		};
+
+	public:
 		TerrainTools();
 
 		virtual const char* GetName() override { return "Terrain Tools"; }
@@ -15,10 +25,12 @@ namespace Editor
 		virtual void DrawImGui() override;
 
 	private:
+		bool _brushTypes[BrushTypes::COUNT] = { false };
+
 		f32 _brushHardness = 100.0f;
 		f32 _brushRadius = 10.0f;
 		f32 _brushPressure = 100.0f;
 
-		ImVec4 _vertexColor = { 0.0f, 0.0f, 0.0f, 1.0f };
+		Color _vertexColor = { 0.0f, 0.0f, 0.0f, 1.0f };
 	};
 }
